cbf5e8f12820d2141320762c57780f989b3b45f4
MO-AI <motodera@HiromiMotodera-no-MacBook-Pro.local>
2015-04-04 14:46:55 +0900

    Add Swift demo project

---
+++ MOAspectsDemoSwift/MOAspectsDemoSwift/Classes/MOAspectsDemoSwift-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  MOAspectsDemoSwift-Bridging-Header.h
+//  MOAspectsDemoSwift
+//
+//  Created by HiromiMotodera on 2015/04/04.
+//  Copyright (c) 2015年 HiromiMotodera. All rights reserved.
+//
+
+#import "MOAspects.h"
